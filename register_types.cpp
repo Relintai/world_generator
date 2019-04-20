7b711b79b57a060a1fe33ee656fb1381b2c3188a
Relintai <relintai@gmail.com>
2019-04-20 14:51:33 +0200

    Initial commit.

---
+++ register_types.cpp
@@ -0,0 +1,10 @@
+#include "register_types.h"
+
+void register_dungeon_generator_types() {
+	//ClassDB::register_class<BSInputModifier>();
+}	
+
+void unregister_dungeon_generator_types() {
+
+}
+
---
+++ register_types.h
@@ -0,0 +1,3 @@
+
+void register_dungeon_generator_types();
+void unregister_dungeon_generator_types();
