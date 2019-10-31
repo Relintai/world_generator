b6b5708003b67e253c311ef9d44cef192bde9ad2
Relintai <relintai@gmail.com>
2019-10-31 01:53:45 +0100

    Fix mistake with bindings.

--- main/dungeon_corridor.cpp
+++ main/dungeon_corridor.cpp
@@ -31,29 +31,6 @@ int DungeonCorridor::get_dungeon_room_count() const {
 	return _dungeon_rooms.size();
 }
 
-void DungeonCorridor::setup() {
-	if (has_method("_setup")) {
-		call("_setup");
-	}
-}
-
-void DungeonCorridor::generate_chunk(Ref<VoxelChunk> chunk, bool spawn_mobs) {
-	if (has_method("_generate_chunk")) {
-		call("_generate_chunk", chunk, spawn_mobs);
-	}
-}
-
-void DungeonCorridor::generate_structure(Ref<VoxelStructure> structure, bool spawn_mobs) {
-	if (has_method("_generate_structure")) {
-		call("_generate_structure", structure, spawn_mobs);
-	}
-}
-
-Ref<Image> DungeonCorridor::generate_map() {
-	ERR_FAIL_COND_V(!has_method("_generate_map"), Ref<Image>());
-
-	return call("_generate_map");
-}
 
 DungeonCorridor::DungeonCorridor() {
 	_max_connections = 2;
@@ -63,14 +40,6 @@ DungeonCorridor::~DungeonCorridor() {
 }
 
 void DungeonCorridor::_bind_methods() {
-	BIND_VMETHOD(MethodInfo("_setup"));
-	BIND_VMETHOD(MethodInfo("_generate_structure", PropertyInfo(Variant::OBJECT, "structure", PROPERTY_HINT_RESOURCE_TYPE, "VoxelStructure"), PropertyInfo(Variant::BOOL, "spawn_mobs")));
-	BIND_VMETHOD(MethodInfo("_generate_chunk", PropertyInfo(Variant::OBJECT, "structure", PROPERTY_HINT_RESOURCE_TYPE, "VoxelChunk"), PropertyInfo(Variant::BOOL, "spawn_mobs")));
-
-	ClassDB::bind_method(D_METHOD("setup"), &DungeonCorridor::setup);
-	ClassDB::bind_method(D_METHOD("generate_chunk", "chunk", "spawn_mobs"), &DungeonCorridor::generate_chunk);
-	ClassDB::bind_method(D_METHOD("generate_structure", "structure", "spawn_mobs"), &DungeonCorridor::generate_structure);
-
 	ClassDB::bind_method(D_METHOD("get_max_connections"), &DungeonCorridor::get_max_connections);
 	ClassDB::bind_method(D_METHOD("set_max_connections", "value"), &DungeonCorridor::set_max_connections);
 	ADD_PROPERTY(PropertyInfo(Variant::INT, "max_connections"), "set_max_connections", "get_max_connections");
@@ -82,8 +51,4 @@ void DungeonCorridor::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("remove_dungeon_room", "index"), &DungeonCorridor::remove_dungeon_room);
 
 	ClassDB::bind_method(D_METHOD("get_dungeon_room_count"), &DungeonCorridor::get_dungeon_room_count);
-
-	BIND_VMETHOD(MethodInfo(PropertyInfo(Variant::OBJECT, "image", PROPERTY_HINT_RESOURCE_TYPE, "Image"), "_generate_map"));
-
-	ClassDB::bind_method(D_METHOD("generate_map"), &DungeonCorridor::generate_map);
 }
