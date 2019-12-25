63da1cc4a2084d1c2eb9430e436be22711e964f2
Relintai <relintai@gmail.com>
2019-12-25 14:08:42 +0100

    Doc setup.

--- config.py
+++ config.py
@@ -3,3 +3,23 @@ def can_build(env, platform):
 
 def configure(env):
     pass
+
+def get_doc_classes():
+    return [
+        "BiomeData",
+        "DungeonCorridorData",
+        "DungeonData",
+        "DungeonRoomData",
+        "PlanetData",
+        "WorldGeneratorPropData",
+        "BiomeData",
+
+        "Planet",
+        "Dungeon",
+        "DungeonRoom",
+        "DungeonCorridor",
+        "Biome",
+    ]
+
+def get_doc_path():
+    return "doc_classes"
